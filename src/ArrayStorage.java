2a6edc5a13d66cdc7ee51e4f79d9ec3c6d371d3b
Kotoman1999 <35837562+Kotoman1999@users.noreply.github.com>
2018-10-02 11:11:32 +0300

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,48 +1,49 @@
 import java.util.Arrays;
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+Resume[] storage = new Resume[10000];
 
-    void clear() {
-        Arrays.fill(storage, 0, size,null);
-        size = 0;
-    }
+int size = 0;
 
-    void save(Resume r) {
-        if(size < storage.length){
-            storage[size] = r;
-            size++;
-        }
-    }
+void clear() {
+Arrays.fill(storage, 0, size, null);
+size =0;
+}
 
-    Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                storage[i] = storage[size - 1];
-                return storage[i];
-            }
-        }
-        size--;
-        return null;
+void save(Resume resume) {
+if(size < storage.length ){
+storage[size] = resume;
+size++;
+}
+}
 
-    }
+Resume get(String uuid) {
+if(storage[0].uuid == uuid ){
+return storage[0];
+}
+return null;
+}
 
-    void delete(String uuid) {
-        for (int i = 0; i < size; i ++){
-            if(storage[i].uuid == uuid)
-                storage[i] = storage[size - 1];
-        }
-        size--;
-    }
+void delete(String uuid) {
+for(int i = 0; i < size; i ++) {
+if(storage[i].uuid.equals(uuid)){
+storage[i] = storage[size - 1];
+storage[size -1] = null;
+size--;
+return;
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
+}
+}
 
-    int size() {
-        return size;
-    }
+}
+/**
+* @return array, contains only Resumes in storage (without null)
+*/
+Resume[] getAll() {
+return Arrays.copyOf(storage, size);
+}
+
+int size() {
+return 0;
+}
 }
